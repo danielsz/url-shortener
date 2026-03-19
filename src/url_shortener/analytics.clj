2da29cf22b2853099ec3d1f39f80abefc582159c
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2026-03-19 14:22:38 +0800

    Extract country analytics

--- src/url_shortener/analytics.clj
+++ src/url_shortener/analytics.clj
@@ -6,6 +6,16 @@
             [com.stuartsierra.component :as component]
             [clojure.core.async :as a :refer [chan dropping-buffer <!! sub unsub close! thread]]))
 
+(defn write-country [geoip path group-id remote-addr]
+  (when-let [m (.get @(:client geoip) (java.net.InetAddress/getByName remote-addr) java.util.Map)]
+        (log/debug (.get m "country_code"))
+        (let [country (.get m "country_code")]
+          (when country
+            (redis/wcar nil
+              (redis/hincrby (countries-key path) country 1)
+              (redis/expire  (countries-key path) TTL-ANALYTICS)
+              (redis/hincrby (group-countries-key group-id) country 1)
+              (redis/expire  (group-countries-key group-id) TTL-ANALYTICS))))))
 
 (defn write-analytics! [geoip path group-id remote-addr referer]
   (log/debug path remote-addr referer)
@@ -15,25 +25,13 @@
         (redis/zadd    (ips-key path) (epoch-now) remote-addr)
         (redis/expire  (ips-key path) TTL-ANALYTICS)
         (redis/hincrby (daily-key path) (today) 1)
-        (redis/expire  (daily-key path) TTL-ANALYTICS))
-      (when group-id
-        (redis/wcar nil
-          (redis/hincrby (group-key group-id) "clicks" 1)
-          (redis/zadd    (group-ips-key group-id) (epoch-now) remote-addr)
-          (redis/expire  (group-ips-key group-id) TTL-ANALYTICS)
-          (redis/hincrby (group-daily-key group-id) (today) 1)
-          (redis/expire  (group-daily-key group-id) TTL-ANALYTICS)))
-      (when-let [m (.get @(:client geoip) (java.net.InetAddress/getByName remote-addr) java.util.Map)]
-        (log/debug (.get m "country_code"))
-        (let [country (.get m "country_code")]
-          (when country
-            (redis/wcar nil
-              (redis/hincrby (countries-key path) country 1)
-              (redis/expire  (countries-key path) TTL-ANALYTICS))
-            (when group-id
-              (redis/wcar nil
-                (redis/hincrby (group-countries-key group-id) country 1)
-                (redis/expire  (group-countries-key group-id) TTL-ANALYTICS)))))))
+        (redis/expire  (daily-key path) TTL-ANALYTICS)
+        (redis/hincrby (group-key group-id) "clicks" 1)
+        (redis/zadd    (group-ips-key group-id) (epoch-now) remote-addr)
+        (redis/expire  (group-ips-key group-id) TTL-ANALYTICS)
+        (redis/hincrby (group-daily-key group-id) (today) 1)
+        (redis/expire  (group-daily-key group-id) TTL-ANALYTICS))
+      (write-country geoip path group-id remote-addr))
     (when referer
       (redis/wcar nil
         (redis/lpush  (referrers-key path) referer)
