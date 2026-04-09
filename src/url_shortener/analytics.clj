d1ab06d938a3fee86ab7a1baa26dbfdb9dffb9dc
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2026-04-09 18:06:09 +0800

    Better referrer detection for Android.

--- src/url_shortener/analytics.clj
+++ src/url_shortener/analytics.clj
@@ -18,41 +18,46 @@
               (redis/hincrby (group-countries-key group-id) country 1)
               (redis/expire  (group-countries-key group-id) TTL-ANALYTICS))))))
 
-
 (defn referrer->platform [referer]
   (if (seq referer)
-    (let [host (try (-> (java.net.URI. referer) .getHost (or ""))
-                    (catch Exception _ ""))]
+    (let [uri    (try (java.net.URI. referer) (catch Exception _ nil))
+          scheme (when uri (.getScheme uri))
+          host   (when uri (or (.getHost uri) ""))]
       (cond
-        (str/blank? host)                          "direct"
-        ;; Twitter — genuine direct clicks
-        (re-find #"(^|\.)reddit\.com$" host)       "reddit"
-        (= host "com.reddit.frontpage")           "reddit"
-        (re-find #"(^|\.)twitter\.com$" host)     "twitter"
-        (= host "t.co")                           "twitter"
-        ;; Twitter — aggregators/alternative browsers (human but indirect)
-        (re-find #"(^|\.)xstalk\.com$" host)      "twitter-aggregator"
-        (re-find #"(^|\.)sotwe\.com$" host)       "twitter-aggregator"
-        (str/starts-with? host "nitter.")         "twitter-aggregator"
-        (= host "cayote.openmtx.com")             "twitter-aggregator"
-        ;; Bluesky — direct and explorers
-        (= host "go.bsky.app")                    "bluesky"
-        (re-find #"(^|\.)bsky\.app$" host)        "bluesky"
-        (re-find #"(^|\.)bluesky\." host)         "bluesky"
-        (= host "tuztoz.com")                     "bluesky"
-        (re-find #"(^|\.)lightnews\.app$" host)   "bluesky"
-        ;; Mastodon / ActivityPub
-        (= host "taboo.cafe")                     "mastodon"
-        (= host "mastodon.social")                "mastodon"
-        ;; Other social
-        (re-find #"(^|\.)facebook\.com$" host)    "facebook"
-        (re-find #"(^|\.)fb\.com$" host)          "facebook"
-        (re-find #"(^|\.)instagram\.com$" host)   "instagram"
-        (re-find #"(^|\.)linkedin\.com$" host)    "linkedin"
-        (= host "com.linkedin.android")             "linkedin"
-        (re-find #"(^|\.)pinterest\." host)       "pinterest"
-        (re-find #"(^|\.)google\." host)          "google"
-        :else                                      "other"))
+        (or (nil? uri) (str/blank? host)) "direct"
+        (= scheme "android-app")
+        (cond
+          (re-find #"\.linkedin\." host)  "linkedin"
+          (re-find #"\.reddit\."   host)  "reddit"
+          (re-find #"\.twitter\."  host)  "twitter"
+          (re-find #"\.instagram\." host) "instagram"
+          (re-find #"\.facebook\." host)  "facebook"
+          (re-find #"\.pinterest\." host) "pinterest"
+          (re-find #"\.youtube\."  host)  "youtube"
+          (re-find #"\.gm$"        host)  "email"
+          (re-find #"\.gmail\."    host)  "email"
+          :else                           "other")
+        (re-find #"(^|\.)reddit\.com$"    host) "reddit"
+        (re-find #"(^|\.)twitter\.com$"   host) "twitter"
+        (= host "t.co")                         "twitter"
+        (re-find #"(^|\.)xstalk\.com$"    host) "twitter-aggregator"
+        (re-find #"(^|\.)sotwe\.com$"     host) "twitter-aggregator"
+        (str/starts-with? host "nitter.")       "twitter-aggregator"
+        (= host "cayote.openmtx.com")           "twitter-aggregator"
+        (= host "go.bsky.app")                  "bluesky"
+        (re-find #"(^|\.)bsky\.app$"      host) "bluesky"
+        (re-find #"(^|\.)bluesky\."       host) "bluesky"
+        (= host "tuztoz.com")                   "bluesky"
+        (re-find #"(^|\.)lightnews\.app$" host) "bluesky"
+        (= host "taboo.cafe")                   "mastodon"
+        (= host "mastodon.social")              "mastodon"
+        (re-find #"(^|\.)facebook\.com$"  host) "facebook"
+        (re-find #"(^|\.)fb\.com$"        host) "facebook"
+        (re-find #"(^|\.)instagram\.com$" host) "instagram"
+        (re-find #"(^|\.)linkedin\.com$"  host) "linkedin"
+        (re-find #"(^|\.)pinterest\."     host) "pinterest"
+        (re-find #"(^|\.)google\."        host) "google"
+        :else                                   "other"))
     "direct"))
 
 (defn write-analytics! [pubsub geoip {:keys [path group-id remote-addr referer]}]
